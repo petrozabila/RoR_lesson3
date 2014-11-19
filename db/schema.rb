84c68948287b65afa328c8ac15b8099dea35fb35
PetroZabila <Zabilamailbox@mail.ru>
2014-11-19 22:16:39 +0400

    new migrations for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118172345) do
+ActiveRecord::Schema.define(version: 20141119175622) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.string   "tags"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
